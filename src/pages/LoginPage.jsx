01170fdd6f46dbb2e90bb89a74a8a1c4ca0b4362
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-12 18:39:14 +0200

    FEAT: Login and Authentication but still needs work

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { useState } from "react";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { SignInPage } from "@toolpad/core/SignInPage";
+import { useTheme } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+function LoginPage() {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const providers = [{ id: "credentials", name: "Email and Password" }];
+  const signIn = async (provider, formData) => {
+    try {
+      const userCredentials = Object.fromEntries(formData.entries());
+
+      const response = await axios.post(
+        `${VITE_SERVER_URL}/api/auth/login`,
+        userCredentials
+      );
+
+      console.log("Login success:", response);
+      localStorage.setItem("authToken", response.data.authToken)
+      navigate("/orders");
+      return;
+    } catch (error) {
+      await new Promise((resolve) => setTimeout(resolve, 500)); // resets the login button on a failed attempt
+      return {
+        error: "Incorrect username or password",
+      };
+    }
+  };
+
+  return (
+    // preview-start
+    <AppProvider theme={theme}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{
+          emailField: { autoFocus: false },
+          form: { noValidate: true },
+        }}
+      />
+    </AppProvider>
+    // preview-end
+  );
+}
+
+export default LoginPage;
