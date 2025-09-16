c82ad2a01d64d6d63e684db4e853cc678acd78c2
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-16 14:45:54 +0200

    FEAT: Inventory table page

---
+++ src/components/InventoryTable.jsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { NavLink } from "react-router-dom";
+
+
+
+function InventoryTable({ rows }) {
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = date.toLocaleString("en-US", { month: "short" }); // Jan, Feb, Mar ...
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead
+          sx={(theme) => ({
+            "& th": {
+              fontWeight: "bold",
+              color: theme.palette.common.white,
+              backgroundColor: theme.palette.primary.main, 
+            },
+          })}
+        >
+          <TableRow>
+            <TableCell>SKU</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Cost</TableCell>
+            <TableCell align="right">Available Qty</TableCell>
+            <TableCell align="right">Location</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row._id}>
+              <TableCell to={`/orders/${row._id}`} scope="row" component={NavLink} >{row.sku}</TableCell>
+              <TableCell align="right">{row.title}</TableCell>
+              <TableCell align="right">{row.cost}</TableCell>
+              <TableCell align="right">{row.availableQty}</TableCell>
+              <TableCell align="right">{row.location}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+export default InventoryTable
--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,6 +1,59 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Box,
+  Stack,
+  TextField,
+  Button,
+  CircularProgress,
+  Typography,
+  Toolbar,
+} from "@mui/material";
+import PageHeader from "../components/PageHeader";
+import InventoryTable from "../components/InventoryTable";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { NavLink } from "react-router-dom";
+
+const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
 function InventoryPage() {
+
+  const { isLoggedIn } = useContext(AuthContext);
+  const [inventory, setInventory] = useState([]);
+
+  const getInventory = async () => {
+    const storedToken = localStorage.getItem("authToken");
+    try {
+      const res = await axios.get(`${VITE_SERVER_URL}/api/inventory`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      });
+      setInventory(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getInventory();
+  }, []);
+
   return (
-    <div>InventoryPage</div>
-  )
+    <Box sx={{ p: 2, pt: 0 }}>
+      <Toolbar />
+      <Stack direction="row" spacing={2} alignItems="center" sx={{ mb: 2 }}>
+        <TextField size="small" label="Search SKU, title)" fullWidth />
+        <Button variant="outlined" onClick={getInventory}>
+          Refresh
+        </Button>
+        <Button variant="contained" component={NavLink} to="/inventory/new">
+          New
+        </Button>
+      </Stack>
+
+      <InventoryTable rows={inventory} />
+    </Box>
+  );
 }
+
 export default InventoryPage
